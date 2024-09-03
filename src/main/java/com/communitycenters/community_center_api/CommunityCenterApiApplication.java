a747b20b8c6f31481fd59647194bb0fefa80d53e
augustogs <augusto.santos@ccc.ufcg.edu.br>
2024-09-03 15:39:18 -0300

    first commit

---
+++ src/main/java/com/communitycenters/community_center_api/CommunityCenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.communitycenters.community_center_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityCenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityCenterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/communitycenters/community_center_api/CommunityCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communitycenters.community_center_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
